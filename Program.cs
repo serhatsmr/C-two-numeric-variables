a671d2a06f522bc587802970a1b0f013ece1771d
serhatsmr <56018259+serhatsmr@users.noreply.github.com>
2020-01-19 21:41:30 +0300

    commit

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace proje
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 1;
+            int b = 2;
+            a = a + b;
+            b = a - b;
+            a = a - b;
+            Console.WriteLine("The value of the 1st variable:{0}", a);
+            Console.WriteLine("The value of the 2nd variable:{0}", b);
+            Console.ReadLine();
+        }
+    }
+}
